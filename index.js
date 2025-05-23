89e503e1f5b7b57999b889cd4203855c8a23ea03
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-23 11:46:00 +0600

    deploy server to vercel

--- index.js
+++ index.js
@@ -41,6 +41,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/employee", async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime,
+        },
+      };
+
+      const result = await orkidEmployee.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
     // app.get("/hobbies/:id", async (req, res) => {
     //   const id = req.params.id;
     //   const query = { _id: new ObjectId(id) };
